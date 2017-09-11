4f7e84f4591967c991ee01fd3c881ffff72b2aca
Sebastian Puente <sebas7pr@gmail.com>
2017-09-11 12:08:39 -0500

    Agrega logica para incrementar contador, guardar y restaurar su estado

--- app/src/main/java/uio/androidbootcamp/activitiessaveandrestorestate/MainActivity.java
+++ app/src/main/java/uio/androidbootcamp/activitiessaveandrestorestate/MainActivity.java
@@ -2,12 +2,46 @@ package uio.androidbootcamp.activitiessaveandrestorestate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private int firstTeamCounter;
+    private int secondTeamCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            firstTeamCounter = savedInstanceState.getInt("firstTeamCounter");
+            secondTeamCounter = savedInstanceState.getInt("secondTeamCounter");
+        }
+
+        Button firstTeamButton = findViewById(R.id.button_first_team);
+        firstTeamButton.setText(String.valueOf(firstTeamCounter));
+        Button secondTeamButton = findViewById(R.id.button_second_team);
+        secondTeamButton.setText(String.valueOf(secondTeamCounter));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("firstTeamCounter", firstTeamCounter);
+        outState.putInt("secondTeamCounter", secondTeamCounter);
+        super.onSaveInstanceState(outState);
+    }
+
+    public void firstTeamIncrement(View view) {
+        firstTeamCounter++;
+        Button firstTeamButton = findViewById(R.id.button_first_team);
+        firstTeamButton.setText(String.valueOf(firstTeamCounter));
+    }
+
+    public void secondTeamIncrement(View view) {
+        secondTeamCounter++;
+        Button secondTeamButton = findViewById(R.id.button_second_team);
+        secondTeamButton.setText(String.valueOf(secondTeamCounter));
     }
 }
